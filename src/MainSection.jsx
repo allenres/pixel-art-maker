d819c53aa6e9cbb314562aa765874ab5d7474a47
allenres <allen.resulidze@gmail.com>
2026-05-05 10:57:24 -0700

    get start on main section

---
+++ src/MainSection.jsx
@@ -0,0 +1,22 @@
+// import { useState } from "react";
+import "./MainSectionstyle.css";
+
+export default function MainSection() {
+    // const [color, setColor] = useState("#000");
+
+    return (
+        <>
+            <h1>Pixel Art Editor</h1>
+            <div>
+                <div className="colorPicker">
+                    <h3>COLOR</h3>
+                    <input type="color" />
+                    <input type="radio" />
+                    <button>Clear</button>
+                    <p>Click any cell to paint it with the current color.</p>
+                </div>
+                <div className="graph"></div>
+            </div>
+        </>
+    )
+}
