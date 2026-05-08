bdf0b40570602ebbebed8389977a9838c0f719cd
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-05-08 09:49:30 -0700

    Set color in useState

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -1,13 +1,21 @@
 // import { useState } from "react";
 import "./MainSection.css";
 import ColorPicker from "./ColorPicker";
-import "./MainSection.css";
-
+import {useState} from 'react'
+import squares from './squares.js'
 
 export default function MainSection() {
     // const [color, setColor] = useState("#000");
     const cells = Array.from({ length: 256 });
-
+    const [color, setColor] = useState("#FFFFFF");
+    
+     const boxes = squares.map((el, index) => (<button 
+        key={index} 
+        className='color-box'
+        style={{backgroundColor: el.color}}
+        onClick={() => setColor(el.color)}
+        >
+        </button>))
     return (
         <>
             <h1>Pixel Art Editor</h1>
@@ -15,10 +23,10 @@ export default function MainSection() {
                 <div className="colorPicker">
                     <h3>COLOR</h3>
                     <input type="color" />
-                    <div className="color-picker">
-                        <ColorPicker />
+                    <div className='box-container'>
+                        {boxes}
                     </div>
-                    
+               
                     <button>Clear</button>
                     <p>Click any cell to paint it with the current color.</p>
                 </div>
